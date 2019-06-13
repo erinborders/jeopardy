9aa6c552fd107dab80bf492a0bf4e03161fc174c
Erin Borders <borders16@gmail.com>
2019-06-13 12:58:17 -0400

    changed functions to use event propogation to delete question tiles, reveal questions, and reveal answers

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-let category1Question1 = $('#category1-question1 h1').eq(0)
-let question = $('#category1-question1 p').eq(0)
-let answers = $('#category1-question1-answers').eq(0)
+// let category1Question1 = $('#category1-question1 h1').eq(0)
+let questionTile = $('.question-tile')
+let question = $('.question-tile p')
+// let answers = $('#answer-field')
 
 //create a function to hide the question
 function hideQuestion () {
@@ -10,17 +11,18 @@ function hideQuestion () {
 hideQuestion();
 
 //create a function to delete the question tile with the point value
-function deleteQuestionTile () {
-    category1Question1.remove()
-}
+// function deleteQuestionTile (e) {
+//     e.target.children.remove()
+// }
 
 //create a function to reveal the question
-function revealQuestion () {
-    question.show();
-}
+// function revealQuestion () {
+//     question.show();
+// }
 
 //create a function to hide the answers
 function hideAnswers () {
+    let answers = $('#answer-field div')
     answers.hide();
 }
 hideAnswers();
@@ -28,12 +30,17 @@ hideAnswers();
 //create a function to reveal the answers
 
 function revealAnswers () {
-    answers.show();
+    let answerID = event.target.parentNode.id
+    let thisAnswer = $('#answer-field #' + answerID)
+    console.log(thisAnswer)
+    thisAnswer.show();
 }
 
 //create event listener to delete question tile and reveal the question
-category1Question1.on('click', function () {
-    deleteQuestionTile();
-    revealQuestion();
+questionTile.on('click', function (evt) {
+    $(this).children().eq(1).show();
+    //reveal answer buttons
     revealAnswers();
+    //removes question tile
+    $(this).children().eq(0).remove();  
 })
