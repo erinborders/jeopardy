e73ba16adb004b1d758faadca4ee90ea5c8e28d9
Erin Borders <borders16@gmail.com>
2019-06-13 09:57:24 -0400

    Preliminary testing of functions that will reveal the question. It worked!

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// console.log('connected')
+
+let category1Question1 = $('#category1-question1 h1').eq(0)
+let question = $('#category1-question1 p').eq(0)
+
+//create a function to hide the question
+function hideQuestion () {
+    question.hide();
+}
+hideQuestion();
+
+//create a function to delete the question tile point value
+function deleteQuestionTile () {
+    category1Question1.remove()
+}
+
+//create a function to reveal the question
+function revealQuestion () {
+    question.show();
+}
+
+//create event listener to delete question tile and reveal the question
+category1Question1.on('click', function () {
+    deleteQuestionTile();
+    revealQuestion();
+})
