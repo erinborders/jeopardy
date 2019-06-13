bb97b555ede82ee7206c80d4b45a2da003a3e910
Erin Borders <borders16@gmail.com>
2019-06-13 10:06:44 -0400

    Preliminary testing of hide/reveal answers functions. They worked and worked with the click event listener

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let category1Question1 = $('#category1-question1 h1').eq(0)
 let question = $('#category1-question1 p').eq(0)
+let answers = $('#category1-question1-answers').eq(0)
 
 //create a function to hide the question
 function hideQuestion () {
@@ -8,7 +9,7 @@ function hideQuestion () {
 }
 hideQuestion();
 
-//create a function to delete the question tile point value
+//create a function to delete the question tile with the point value
 function deleteQuestionTile () {
     category1Question1.remove()
 }
@@ -18,8 +19,21 @@ function revealQuestion () {
     question.show();
 }
 
+//create a function to hide the answers
+function hideAnswers () {
+    answers.hide();
+}
+hideAnswers();
+
+//create a function to reveal the answers
+
+function revealAnswers () {
+    answers.show();
+}
+
 //create event listener to delete question tile and reveal the question
 category1Question1.on('click', function () {
     deleteQuestionTile();
     revealQuestion();
+    revealAnswers();
 })
